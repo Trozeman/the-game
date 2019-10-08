af8df935c3c30b2e687dde05421fbbe2c5878d23
Dima Shymko <trozeman@gmail.com>
2019-10-08 20:59:53 +0300

    [-IMPLEMENT-] Server body parser && Fake database ¯\_(ツ)_/¯

--- server/index.js
+++ server/index.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/game-settings', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
+
+class DB {
+    constructor(initialData) {
+        this.data = initialData;
+    }
+    get leaderBoard() {
+        return this.data.players
+    }
+    get options() {
+        return this.data.options
+    }
+
+    addWinner(name) {
+        this.data.players.push(
+            {
+                name,
+                date: new Date()
+            }
+        )
+    }
+}
+
+const initialData = {
+    options: {
         field: [3, 5, 7, 9],
-        delay : [1000, 700, 500, 300]
-    }));
-});
-app.get('/winners', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify([
+        delay: [1000, 700, 500, 300]
+    },
+    players: [
         {
             name: "Firs",
             date: new Date()
@@ -21,8 +42,30 @@ app.get('/winners', (req, res) => {
             name: "Nik",
             date: new Date()
         }
-    ]));
+    ]
+};
+
+const dataBase = new DB(initialData);
+
+app.post('/winners', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    dataBase.addWinner(req.body.name);
+    res.status(201);
+    res.send(JSON.stringify(dataBase.leaderBoard));
 });
+
+app.get('/game-settings', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200);
+    res.send(JSON.stringify(dataBase.options));
+});
+
+app.get('/winners', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200);
+    res.send(JSON.stringify(dataBase.leaderBoard));
+});
+
 
 app.listen(4000, () =>
     console.log('Express server is running on localhost:4000')
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import configureStore from './store';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import {renderToString} from 'react-dom/server';
+
 const initialState = {
     game: {
         user: '',
@@ -24,6 +25,22 @@ const store = configureStore(initialState);
 window.store = store;
 
 function App() {
+
+
+    fetch('/winners', {
+        method: 'POST',
+        mode: 'cors',
+        referrer: 'no-referrer',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name: "POST TEST"}),
+        test: 123
+    })
+        .then(response => response.json())
+        .then(e => console.log(e));
+
+
     fetch('/game-settings')
         .then(response => response.json())
         .then(e => console.log(e));
@@ -33,13 +50,13 @@ function App() {
     return (
         <Provider store={store}>
             <Router>
-                <Route path="/home" render={() => renderToString('{"DATA": OK}')} />
+                <Route path="/home" render={() => renderToString('{"DATA": OK}')}/>
                 <Route path="/game-settings">
                     <h1>
                         game-settings
                     </h1>
                 </Route>
-                <Route path="/" exact >
+                <Route path="/" exact>
                     <GridContainer store={store}/>
                 </Route>
             </Router>
